2cb46bd847d3fe60cc684f00e27ce1e690ac6a9f
Ahmed Tarek <122125653+ahmed-tarek-2004@users.noreply.github.com>
2024-03-30 21:15:21 +0200

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -16,7 +16,6 @@ bool isSafe(int board[N][N], int row, int col, int num)
         if (board[i][col] == num)
             return false;
 
-    
     int boxRowStart = row - row % 3;
     int boxColStart = col - col % 3;
 
@@ -30,77 +29,67 @@ bool isSafe(int board[N][N], int row, int col, int num)
 
 void printBoard(int grid[N][N])
 {
-
     system("cls");
     cout << "\t\t\t<================================================================================>" << endl;
     cout << "\t\t\t|                        WELCOME TO SUDOKU Game!                                 |" << endl;
-    cout << "\t\t\t|       Fill in the missing numbers(represented by 0) to solve the puzzle.       |" << endl;
+    cout << "\t\t\t|       Fill in the missing numbers (represented by 0) to solve the puzzle.      |" << endl;
     cout << "\t\t\t<================================================================================>" << endl;
     for (int row = 0; row < N; row++)
     {
         for (int col = 0; col < N; col++)
         {
-            if(col == 3 || col == 6)
+            if (col == 3 || col == 6)
                 cout << " | ";
-            cout << grid[row][col] <<" ";
+            cout << grid[row][col] << " ";
         }
-        if(row == 2 || row == 5)
+        if (row == 2 || row == 5)
         {
             cout << endl;
-            for(int i = 0; i<N; i++)
+            for (int i = 0; i < N; i++)
                 cout << "---";
         }
         cout << endl;
     }
 }
 
-void fillRandomNumbers(int board[N][N])
+void fillRandomNumbers(int board[N][N], int numHints)
 {
     srand(time(0));
 
     // Fill the board with random numbers ensuring each number doesn't repeat in its row or column
-    for (int num = 1; num <= N; num++)
+    for (int count = 0; count < numHints; count++)
     {
-        for (int count = 0; count < N; count++)
+        int row = rand() % N;
+        int col = rand() % N;
+        int num = rand() % N + 1;
+
+        // Find a random empty cell
+        while (board[row][col] != 0 || !isSafe(board, row, col, num))
         {
-            int row = rand() % N;
-            int col = rand() % N;
-
-            // Find a random empty cell
-            while (board[row][col] != 0)
-            {
-                row = rand() % N;
-                col = rand() % N;
-            }
-
-           if (isSafe(board, row, col, num))
-            {
-                board[row][col] = num;
-                break;
-            }
+            row = rand() % N;
+            col = rand() % N;
+            num = rand() % N + 1;
         }
+
+        board[row][col] = num;
     }
 }
 
 bool solveSudoku(int board[N][N], int row, int col)
 {
-    
     if (row == N - 1 && col == N)
         return true;
 
-   
     if (col == N)
     {
         row++;
         col = 0;
     }
 
-    
     if (board[row][col] != 0)
         return solveSudoku(board, row, col + 1);
 
-    
-    for (int num = 1; num <= 9; num++)
+    for (int num = 1; num <= N; num++)
     {
         if (isSafe(board, row, col, num))
         {
@@ -127,13 +116,12 @@ bool isSolvedCompletely(int grid[N][N])
 
 void playGame(int board[N][N])
 {
-    int ch;
     int row, col, num;
-    while(true)
+    while (true)
     {
         printBoard(board);
         cout << endl << endl;
-        cout << "Unable to solve? Enter -1 as row, col and num to view the solved sudoku."<<endl;
+        cout << "Unable to solve? Enter -1 as row, col, and num to view the solved sudoku." << endl;
         cout << "Enter row: ";
         cin >> row;
         cout << "Enter column: ";
@@ -141,7 +129,7 @@ void playGame(int board[N][N])
         cout << "Enter number: ";
         cin >> num;
 
-        if(row == -1 || col == -1 || num == -1)
+        if (row == -1 || col == -1 || num == -1)
         {
             solveSudoku(board, 0, 0);
             printBoard(board);
@@ -161,19 +149,7 @@ void playGame(int board[N][N])
         board[row][col] = num;
     }
 
-    
-    bool solved = true;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (board[i][j] == 0)
-            {
-                solved = false;
-                break;
-            }
-        }
-    }
+    bool solved = isSolvedCompletely(board);
 
     if (solved)
     {
@@ -184,70 +160,85 @@ void playGame(int board[N][N])
     {
         cout << "Puzzle not solved. Better luck next time." << endl;
     }
-
 }
 
 int main()
 {
-
-    system("title Sudoku Game @copyassignment");
+    system("title Sudoku Game");
     system("color B0");
     int board[N][N];
-    memset(board, 0, sizeof(board)); 
-    fillRandomNumbers(board);
+    memset(board, 0, sizeof(board));
 
-    cout << "\t\t\t<================================================================================>" << endl;
-    cout << "\t\t\t|                        WELCOME TO SUDOKU Game!                                 |" << endl;
-    cout << "\t\t\t|       Fill in the missing numbers(represented by 0) to solve the puzzle.       |" << endl;
-    cout << "\t\t\t<================================================================================>" << endl;
 
     while (true)
     {
+    cout << "\t\t\t<================================================================================>" << endl;
+    cout << "\t\t\t|                        WELCOME TO SUDOKU Game!                                 |" << endl;
+    cout << "\t\t\t|       Fill in the missing numbers (represented by 0) to solve the puzzle.      |" << endl;
+    cout << "\t\t\t<================================================================================>" << endl;
         memset(board, 0, sizeof(board));
         int choice;
         cout << endl << endl;
-        cout << "\t\t[1] Solve the Sudoku" << endl;
-        cout << "\t\t[2] Unable to solve? View the solved Sudoku" << endl;
-        cout << "\t\t[3] Exit" << endl;
-        cout << "\t\tEnter your choice: ";
+        cout << "\t\t\t\t[1] Solve the Sudoku" << endl;
+        cout << "\t\t\t\t[2] Unable to solve? View the solved Sudoku" << endl;
+        cout << "\t\t\t\t[3] Exit" << endl;
+        cout << "\t\t\t\tEnter your choice: ";
         cin >> choice;
 
         switch (choice)
         {
         case 1:
+            int difficulty;
+            cout << "Choose difficulty level:" << endl;
+            cout << "1. Easy" << endl;
+            cout << "2. Medium" << endl;
+            cout << "3. Hard" << endl;
+            cout << "Enter your choice: ";
+            cin >> difficulty;
+
+            int numHints;
+            if (difficulty == 1)
+            {
+                numHints = 7;
+            }
+            else if (difficulty == 2)
+            {
+                numHints = 5;
+            }
+            else if (difficulty == 3)
+            {
+                numHints = 3;
+            }
+            else
+            {
+                cout << "Invalid difficulty level!" << endl;
+                continue;
+            }
+
+            fillRandomNumbers(board, numHints);
             playGame(board);
             break;
+
         case 2:
             if (solveSudoku(board, 0, 0))
             {
-                cout << "Completely Solved Sudoku is: "<< endl;
-                cout << endl << endl;
-                for (int row = 0; row < N; row++)
-                {
-                    for (int col = 0; col < N; col++)
-                    {
-                        if(col == 3 || col == 6)
-                            cout << " | ";
-                        cout << board[row][col] <<" ";
-                    }
-                    if(row == 2 || row == 5)
-                    {
-                        cout << endl;
-                        for(int i = 0; i<N; i++)
-                            cout << "---";
-                    }
-                    cout << endl;
-                }
+                cout << "Completely Solved Sudoku is: " << endl;
+                printBoard(board);
                 cout << endl;
                 cout << "Better luck next time!!!" << endl;
             }
             else
                 cout << "No solution found" << endl;
             break;
+
         case 3:
+            cout << "\t\t\t\tThanks\n\n\n";
             exit(0);
+
         default:
             cout << "Invalid choice" << endl;
         }
+        system("CLS");
     }
+    return 0;
 }
